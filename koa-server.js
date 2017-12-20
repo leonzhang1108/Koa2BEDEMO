07f9dd66df7b56ecef08400f9a653e798e9e6476
leon <jianliangzhang@ctrip.com>
2017-12-20 18:51:22 +0800

    子路由

--- koa-server.js
+++ koa-server.js
@@ -1,5 +1,6 @@
 import Koa from 'koa'
 import logger from './middleware/logger-async'
+import Router from 'koa-router'
 import {
   render
 } from './utils/util'
@@ -7,42 +8,38 @@ const app = new Koa()
 
 
 
-/**
- * 根据URL获取HTML内容
- * @param  {string} url koa2上下文的url，ctx.url
- * @return {string}     获取HTML文件内容
- */
-async function route(url) {
-  let view = '404.html'
-  switch (url) {
-    case '/':
-      view = 'index.html'
-      break
-    case '/index':
-      view = 'index.html'
-      break
-    case '/todo':
-      view = 'todo.html'
-      break
-    case '/404':
-      view = '404.html'
-      break
-    default:
-      break
-  }
-  let html = await render(view)
-  return html
-}
-
 // middleware 
 app.use(logger())
 
-// response 
-app.use(async ctx => {
-  let url = ctx.request.url
-  let html = await route(url)
+let home = new Router()
+
+// 子路由1
+home.get('/', async ( ctx )=>{
+  let html = `
+    <ul>
+      <li><a href="/page/helloworld">/page/helloworld</a></li>
+      <li><a href="/page/404">/page/404</a></li>
+    </ul>
+  `
   ctx.body = html
 })
+
+// 子路由2
+let page = new Router()
+page.get('/404', async ( ctx )=>{
+  ctx.body = '404 page!'
+}).get('/helloworld', async ( ctx )=>{
+  ctx.body = 'helloworld page!'
+})
+
+// 装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+
 
 app.listen(3000, err => {
   if (err) {
