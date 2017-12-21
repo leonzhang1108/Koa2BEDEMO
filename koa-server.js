fc8d13dd16677f25d7bc2e30576c27254eea8569
leon <jianliangzhang@ctrip.com>
2017-12-21 10:21:13 +0800

    koa-static 静态服务器

--- koa-server.js
+++ koa-server.js
@@ -1,7 +1,9 @@
 import Koa from 'koa'
 import logger from './middleware/logger-async'
 import Router from 'koa-router'
+import path from 'path'
 import bodyParser from 'koa-bodyparser'
+import koa_static from 'koa-static'
 import {
   render
 } from './utils/util'
@@ -14,31 +16,12 @@ app.use(logger())
 app.use(bodyParser())
 
 // response 
-app.use(async ctx => {
-  if ( ctx.url === '/' && ctx.method === 'GET' ) {
-    // 当GET请求时候返回表单页面
-    let html = `
-      <h1>koa2 request post demo</h1>
-      <form method="POST" action="/">
-        <p>userName</p>
-        <input name="userName" /><br/>
-        <p>nickName</p>
-        <input name="nickName" /><br/>
-        <p>email</p>
-        <input name="email" /><br/>
-        <button type="submit">submit</button>
-      </form>
-    `
-    ctx.body = html
-  } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
-    // 当POST请求的时候，中间件koa-bodyparser解析POST表单里的数据，并显示出来
-    let postData = ctx.request.body
-    console.log(postData)
-    ctx.body = postData
-  } else {
-    // 其他请求显示404
-    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-  }
+app.use(koa_static(
+  path.join(__dirname)
+))
+
+app.use( async ( ctx ) => {
+  ctx.body = 'hello world'
 })
 
 app.listen(3000, err => {
